b889f639b7e533269ee03f6c2776775aed67fd51
Dimitri <loslierdimitri@gmail.com>
2022-09-01 11:46:15 +0200

    add deconnection controller, prepare connection and connection test verification

---
+++ Controller/controller_deconnection.php
@@ -0,0 +1,22 @@
+<?php
+//check session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<?php
+//class
+?>
+
+<?php
+//code
+?>
+
+<?php
+//view
+?>
+
+<?php
+//form post
+?>
--- View/component/all_controller.php
+++ View/component/all_controller.php
@@ -1,3 +1,4 @@
+<p>------------------------------------------------------------------------------------------------</p>
 <a href="/project_trip_redone/Controller/controller_main.php">controller_main</a><br>
 <a href="/project_trip_redone/Controller/controller_account.php">controller_account</a><br>
 <a href="/project_trip_redone/Controller/controller_connection.php">controller_connection</a><br>
@@ -7,3 +8,14 @@
 <a href="/project_trip_redone/Controller/controller_buy.php">controller_buy</a><br>
 <a href="/project_trip_redone/Controller/controller_404.php">controller_404</a><br>
 <a href="/project_trip_redone/TEST.php">TEST</a><br>
+
+<?php
+if (isset($_SESSION["type"]) && isset($_SESSION["id"])) {
+?>
+    <p>id: <?= $_SESSION["id"] ?></p>
+    <p>type: <?= $_SESSION["type"] ?></p>
+<?php
+}
+?>
+
+<p>------------------------------------------------------------------------------------------------</p>

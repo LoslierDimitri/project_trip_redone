91069740230f4d942ab2953df46c8ec4396719b8
LOSLIER Dimitri <loslierdimitri@gmail.com>
2022-09-05 13:39:48 +0200

    add footer css to page_main

--- View/page/page_main.php
+++ View/page/page_main.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/project_trip_redone/View/css/navbar.css">
+    <link rel="stylesheet" href="/project_trip_redone/View/css/footer.css">
     <title>Page main</title>
 </head>
 
